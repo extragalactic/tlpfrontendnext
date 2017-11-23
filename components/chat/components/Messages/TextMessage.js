41152d1c419325188933ffaea95564e6d8171067
JonathonJulian <jfritz50@gmail.com>
2017-11-23 11:26:49 -0800

    update to styles for chatbot

--- components/chat/components/Messages/TextMessage.js
+++ components/chat/components/Messages/TextMessage.js
@@ -1,8 +1,23 @@
-import React, { Component } from 'react';
-import chatIconUrl from './../../assets/chat-icon.svg';
+import React, { Component } from "react";
+import chatIconUrl from "./../../assets/chat-icon.svg";
 
-const TextMessage = (props) => {
-  return <div className="sc-message--text">{props.data.text}</div>
-}
+const TextMessage = props => {
+  return (
+    <div className="sc-message--text">
+      {props.data.text}
+      <style jsx>{`
 
-export default TextMessage
+  .sc-message--text {
+    padding: 17px 20px;
+    border-radius: 6px;
+    font-weight: 300;
+    font-size: 14px;
+    line-height: 1.4;
+    -webkit-font-smoothing: subpixel-antialiased
+  }
+    `}</style>
+    </div>
+  );
+};
+
+export default TextMessage;
