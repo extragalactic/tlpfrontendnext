3abfa91a7c4f2e6966cda818ff05bc42236a360c
John Fritz <johnfritz@Johns-Mac-mini.local>
2017-12-31 13:22:38 -0800

    update amp header

--- components/AmpPageHead.js
+++ components/AmpPageHead.js
@@ -22,6 +22,20 @@ class AmpPageHead extends React.Component {
         <script async custom-template="amp-mustache" src="https://cdn.ampproject.org/v0/amp-mustache-0.1.js"></script>
         <script async custom-element="amp-form" src="https://cdn.ampproject.org/v0/amp-form-0.1.js"></script>
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,400italic|Libre+Baskerville" rel="stylesheet" type="text/css" />
+        <link rel="icon" type="image/png" href="https://s3.ca-central-1.amazonaws.com/3lpm/website/images/favicon.ico"/>
+        <meta property="og:title" content="Three Little Pigs Masonry"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:url" content="http://www.threelittlepigsmasonry.com"/>
+        <meta property="og:image" content="%PUBLIC_URL%/images/social-media/3LPM-og-banner.jpg"/>
+        <meta property="og:image:type" content="image/jpeg" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:site_name" content="Three Little Pigs Masonry"/>
+        <meta property="og:description" content="Three Little Pigs Masonry provides high quality masonry restoration and renovation services for the GTA home and business owner."/>
+        <meta name="twitter:site" content="@3PigsMasonry" />
+        <meta name="twitter:creator" content="@3PigsMasonry" />
+        <meta name="twitter:image" content="%PUBLIC_URL%/images/social-media/3LPM-twitter-card.jpg" />
+        <meta name="twitter:summary_large_image" content="%PUBLIC_URL%/images/social-media/3LPM-twitter-card-large.jpg" />
         <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1" />
         <title>Three Little Pigs Masonry</title>
 
