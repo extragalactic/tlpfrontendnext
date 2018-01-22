29d04964fb60c1746d832a936c54a983f3c89dc0
John Fritz <johnfritz@Johns-Mac-mini.local>
2018-01-22 07:06:59 -0800

    update text

--- components/AboutUs.js
+++ components/AboutUs.js
@@ -39,14 +39,10 @@ const AboutUs = () => {
         </MediaQuery>
         <StyledText>
           <p>
-            Family owned and operated, we have been proudly serving homeowners since 2004. Our masonry history begins
-            with working across Canada and the United States in commercial masonry building schools, churches plus
-            commercial and industrial projects.
+          Family owned and operated, we have been proudly serving homeowners since 2004. Our masonry history begins with working across Canada and the United States in commercial masonry building schools, churches plus commercial and industrial projects.
           </p>
           <p>
-            In 2004, our estimator and partner launched Three Little Pigs Masonry, a full service masonry company for
-            the homeowner. His many years of experience allow him to help homeowners with many permanent solutions to
-            their problems where other competitors have failed to succeed.
+          In 2004, David Fritz founded Three Little Pigs Masonry, a full service masonry company for the homeowner. His many years of experience allow him to help homeowners with many permanent solutions to their problems where other competitors have failed to succeed.
           </p>
         </StyledText>
       </StyledTextwithLogo>
