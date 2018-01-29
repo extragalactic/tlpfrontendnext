925e001b09d994d5a56bb6e8379b9dbea2cf2de4
John Fritz <johnfritz@Johns-Mac-mini.local>
2018-01-29 14:37:48 -0800

    ssl support

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const next = require('next')
+const fs = require('fs');
 const routes = require('./routes')
 const app = next({dev: process.env.NODE_ENV !== 'production'})
 const handler = routes.getRequestHandler(app)
-
-// Without express
-const {createServer} = require('http');
+const cert = fs.readFileSync('./certs/cert.crt').toString();
+const key = fs.readFileSync('./certs/private.key').toString();
+const certA = fs.readFileSync('./certs/gd1.crt').toString();
+const certB = fs.readFileSync('./certs/gd2.crt').toString();
+const certC = fs.readFileSync('./certs/gd3.crt').toString();
+const {createServer} = require('https');
 app.prepare().then(() => {
-  createServer(handler).listen(8080)
+  createServer({key, cert, ca: [certA, certB, certC] }, handler).listen(443)
 })
