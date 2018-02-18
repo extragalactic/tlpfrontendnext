4947fad4311c5f1f24e1f1e0d4cb0919c53a7ed7
John Fritz <johnfritz@Johns-Mac-mini.local>
2018-02-18 08:27:15 -0800

    switch to http

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
-const next = require('next')
+const next = require('next');
+const routes = require('./routes');
+
+const app = next({ dev: process.env.NODE_ENV !== 'production' });
+const handler = routes.getRequestHandler(app);
+
+const { createServer } = require('http');
+
+app.prepare().then(() => {
+  createServer(handler).listen(80);
+});
+
+/*
+//HTTPS
 const fs = require('fs');
-const routes = require('./routes')
-const app = next({dev: process.env.NODE_ENV !== 'production'})
-const handler = routes.getRequestHandler(app)
 const cert = fs.readFileSync('./certs/cert.crt').toString();
 const key = fs.readFileSync('./certs/private.key').toString();
 const certA = fs.readFileSync('./certs/gd1.crt').toString();
 const certB = fs.readFileSync('./certs/gd2.crt').toString();
 const certC = fs.readFileSync('./certs/gd3.crt').toString();
-const {createServer} = require('https');
-app.prepare().then(() => {
-  createServer({key, cert, ca: [certA, certB, certC] }, handler).listen(8443)
-})
+  createServer({ key, cert, ca: [certA, certB, certC] }, handler).listen(8443);
+*/
