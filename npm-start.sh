785f07b2b812a44c76baa5fa96a62e3b49a4a8ed
Jonathon Fritz <jfritz50@gmail.com>
2018-03-13 16:08:11 -0700

    Update npm-start.sh

--- npm-start.sh
+++ npm-start.sh
@@ -1,6 +1,6 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-/home/ubuntu/.nvm/versions/node/v9.5.0/bin/pm2 kill
+pm2 kill
 cd /home/ubuntu/tlpfrontend
 npm run build
-/home/ubuntu/.nvm/versions/node/v9.5.0/bin/pm2 start server.js
+pm2 start server.js
