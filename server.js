98839b04acc333708330e73d7ab4be448982ee09
John Fritz <johnfritz@Johns-Mac-mini.local>
2018-03-16 14:00:29 -0700

    update

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ const handle = routes.getRequestHandler(app);
 app.prepare().then(() => {
   const server = express();
   server.set('trust proxy');
+  server.get('/robots.txt', (req, res) => {
+    res.type('text/plain');
+    res.send('User-agent: *\nDisallow: /');
+  });
+
   server.get('*', (req, res, next) => {
     if (req.headers['x-forwarded-proto'] === 'http') {
       return res.redirect('https://threelittlepigsmasonry.com');
