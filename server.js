34fa200eb53e959be6b8376e8fea175f22447766
John Fritz <johnfritz@Johns-Mac-mini.local>
2018-03-16 12:25:58 -0700

    protocol

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
-const { createServer } = require('http');
-const { parse } = require('url')
 const next = require('next');
 const routes = require('./routes');
+const express = require('express');
 
+const port = 8080;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = routes.getRequestHandler(app);
+
+app.prepare().then(() => {
+  const server = express();
+  server.get('*', (req, res) => {
+    console.log(req.protocol);
+    return handle(req, res);
+  });
+
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
+
+/*
 const app = next({ dev: process.env.NODE_ENV !== 'production' });
 const handler = routes.getRequestHandler(app);
 
 app.prepare().then(() => {
   createServer(handler).listen(8080);
 });
+
+
+*/
