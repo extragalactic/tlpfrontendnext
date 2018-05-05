476bbd5a5db8add3c853f2634a23408764657d36
John Fritz <johnfritz@Johns-Mac-mini.local>
2018-05-04 19:00:59 -0700

    update amp

--- pages/_document.js
+++ pages/_document.js
@@ -70,6 +70,19 @@ export default class MyDocument extends Document {
     return (
       <html amp="">
         <Head>
+          <style amp-boilerplate="">
+            {
+              'body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}'
+            }
+          </style>
+          <noscript>
+            <style amp-boilerplate="">
+              {
+                'body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}'
+              }
+            </style>
+          </noscript>
+
           <title>Three Little Pigs Masonry</title>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link
